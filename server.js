54b0f48165c3183cf9ac23f8dfd4b2f9987f25de
Albert Hermida <alberthermida@gmail.com>
2017-03-26 23:10:05 -0400

    update map get

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 
 
-app.use(express.static(''));
+app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.sendfile(path.join(__dirname, 'public/index.html'));
 });
